1d3232c412d1eff3643de480a8e42d02a6d0449e
0carvalh0 <trabalho.joaovictor2004@gmail.com>
2024-11-10 00:07:49 -0300

    feat: aula 11 - Arquivo de configuração

--- TW_Tiago-Matos/tailwind.config.js
+++ TW_Tiago-Matos/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-purple": {
+          200: "#D9C3FF",
+          300: "#BFA3FF",
+          400: "#A380FF",
+          500: "#8A5DFF",
+        },
+      },
+      spacing: {
+        "extremamentexl": "4rem",
+      }
+    },
   },
   plugins: [],
 }
